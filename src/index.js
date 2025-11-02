968bfd9bd3426961762adf122ee68b151146ebf6
Shriyesh dixit <shriyeshdixit@gmail.com>
2025-11-02 09:07:09 +0530

    DB connection and changes in json file

---
+++ src/index.js
@@ -0,0 +1,51 @@
+// require('dotenv').config()
+import dotenv from 'dotenv'
+import connectDB from './db/index.js'
+
+dotenv.config({
+    path:'./env'
+})
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+creating iffe to connect db in index file it is a good approach , but it pollutes index file
+import express from 'express'
+const app = express()
+
+;(async ()=>{
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error",(error)=>{
+            console.log('Error: ',error);
+            throw err
+            
+        })
+
+        app.listen(process.env.PORT,()=>{
+            console.log(`app is listening on ${process.env.PORT}`);
+        })
+    }catch(error){
+        console.log("error :",error);
+        throw err
+    }
+} )()
+
+*/
