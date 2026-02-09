5fbdc068ef162c51c0bc349533191fdf44b48ae3
Shriyesh Dixit <shriyeshdixit@gmail.com>
2026-02-09 07:00:37 +0530

    final changes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -34,7 +34,7 @@ const registerUser = asynchandler(async (req,res)=>{
         throw new ApiError(400,"All fields are required")
     }
 
-    const existedUser = User.findOne({
+    const existedUser =await User.findOne({
         $or:[{username},{email}]
     })
 
@@ -43,12 +43,17 @@ const registerUser = asynchandler(async (req,res)=>{
     }
 
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length>0){
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
+
 
     if(!avatarLocalPath){
         throw new ApiError(400,"Avatar file is required")
     }
-
+ 
     const avatar = await uploadOnCloudinary(avatarLocalPath) 
     const coverimage= await uploadOnCloudinary(coverImageLocalPath)
 
